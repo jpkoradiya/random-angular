946534c2552c72c7a20159813cd9b19984659225
jpkoradiya <jpkoradiya@myseneca.ca>
2021-03-26 18:57:03 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+function log(){
+  console.log("Hi there");
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-intro';
+
+  constructor(){
+    console.log("Method from the constructor");
+  }
+
+  aSimpleMethod(){
+    console.log("Hi from Simple Method!");
+  }
 }
